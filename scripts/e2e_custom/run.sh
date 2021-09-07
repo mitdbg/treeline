b8cd518029a21f068a44fac675fa98f75b600978
Geoffrey Yu <geoffxy@mit.edu>
2021-09-07 15:21:56 -0400

    Adapt the workload generator invocation scripts to work with LeanStore

--- scripts/e2e_custom/run.sh
+++ scripts/e2e_custom/run.sh
@@ -15,29 +15,45 @@ shift 1
 
 # Evaluates any environment variables in this script's arguments. This script
 # should only be run on trusted input.
-orig_args=$@
+orig_args=($@)
 args=()
 for val in "${orig_args[@]}"; do
-  args+=$(eval "echo $val")
+  phys_arg=$(eval "echo $val")
+  args+=($phys_arg)
+
+  # Extract the database type (e.g., llsm, rocksdb, leanstore)
+  if [[ $phys_arg =~ --db=.+ ]]; then
+    db_type=${phys_arg:5}
+  fi
 done
 
-full_checkpoint_path=$DB_CHECKPOINT_PATH/$checkpoint_name
+echo "Detected DB Type: $db_type"
 
+# Add common arguments.
+args+=("--verbose")
+args+=("--db_path=$DB_PATH")
+args+=("--seed=$SEED")
+
+full_checkpoint_path=$DB_CHECKPOINT_PATH/$checkpoint_name
 rm -rf $DB_PATH
-cp -r $full_checkpoint_path $DB_PATH
+
+# LeanStore does not support "reopening" a database. So we do not have
+# checkpoints for LeanStore. Note that we currently **do not** run the "setup"
+# workload on LeanStore (10 M uniform updates).
+if [[ $db_type == "leanstore" ]]; then
+  mkdir $DB_PATH
+else
+  args+=("--skip_load")
+  cp -r $full_checkpoint_path $DB_PATH
+fi
+
 sync $DB_PATH
 
 set +e
 iostat -o JSON -d -y 1 > $COND_OUT/iostat.json &
 iostat_pid=$!
 
-../../build/bench/run_custom \
-  --verbose \
-  --db_path=$DB_PATH \
-  --seed=$SEED \
-  --skip_load \
-  $args \
-  > $COND_OUT/results.csv
+../../build/bench/run_custom ${args[@]} > $COND_OUT/results.csv
 code=$?
 
 cp $DB_PATH/llsm/LOG $COND_OUT/llsm.log
