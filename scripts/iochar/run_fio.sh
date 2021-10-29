ee65e61e2ea20b820867ab876e136362430d4f2d
Geoffrey Yu <geoffxy@mit.edu>
2021-10-29 14:13:50 -0400

    Script changes to work with the raw block device

--- scripts/iochar/run_fio.sh
+++ scripts/iochar/run_fio.sh
@@ -14,11 +14,23 @@ pushd $script_loc
 source fio_config.sh
 popd
 
-mkdir -p $FIO_OUT_DIR
+device=$1
+jobfile=$2
+shift 2
+
+args=()
+args+=("--output=$COND_OUT/fio.json")
+args+=("--output-format=json")
+if [ $device = "nvme_ssd_raw" ]; then
+  args+=("--directory=$NVME_SSD_RAW_OUT_DIR")
+  args+=("--filename=$NVME_SSD_RAW_FILE")
+else
+  args+=("--directory=$NVME_SSD_FS_OUT_DIR")
+  args+=("--filename=$jobfile")
+  mkdir -p $NVME_SSD_FS_OUT_DIR
+  rm -f $NVME_SSD_FS_OUT_DIR/$jobfile
+fi
+args+=($jobfile)
 
 # All jobs in a jobfile share the same physical file.
-fio \
-  --output=$COND_OUT/fio.json \
-  --output-format=json \
-  --directory=$FIO_OUT_DIR \
-  $@
+fio ${args[@]}
