75d6af9f31683577467cd28206247525fb26affb
Geoffrey Yu <geoffxy@mit.edu>
2021-12-10 10:17:43 -0500

    Record skew type, fix processing script

--- scripts/sim_deferral/combine.py
+++ scripts/sim_deferral/combine.py
@@ -12,12 +12,14 @@ def main():
         for exp in dep.iterdir():
             parts = exp.name.split("-")
             theta = int(parts[2]) / 100
+            skew_type = parts[1]
             df = pd.read_csv(exp / "results.csv")
             df.insert(0, "theta", theta)
+            df.insert(0, "skew_type", skew_type)
             results.append(df)
     
     combined = pd.concat(results, ignore_index=True)
-    combined = combined.sort_values(by=["theta"])
+    combined = combined.sort_values(by=["skew_type", "theta", "flush_fill_threshold"])
     combined.to_csv(out_dir / "results.csv", index=False)
 
 
