36da1c648222034b13a1a63128205b9b08d9a8bf
Markos Markakis <markakis@mit.edu>
2022-01-19 12:19:46 -0500

    Toggle safe/unsafe variants of Put()/GetIndex()

--- tests/record_cache_test.cc
+++ tests/record_cache_test.cc
@@ -14,8 +14,9 @@ TEST(RecordCacheTest, SimplePutGet) {
   rc.Put(key, value);
 
   uint64_t index_out;
-  ASSERT_TRUE(rc.GetIndex(key, &index_out).ok());
+  ASSERT_TRUE(rc.GetIndex(key, false, &index_out).ok());
   ASSERT_EQ(value.compare(rc.cache_entries[index_out].GetValue()), 0);
+  rc.cache_entries[index_out].Unlock();
 }
 
 TEST(RecordCacheTest, SimpleMiss) {
@@ -23,7 +24,7 @@ TEST(RecordCacheTest, SimpleMiss) {
   Slice key = "aaa";
 
   uint64_t index_out;
-  ASSERT_TRUE(rc.GetIndex(key, &index_out).IsNotFound());
+  ASSERT_TRUE(rc.GetIndex(key, false, &index_out).IsNotFound());
 }
 
 TEST(RecordCacheTest, PutVariants) {
@@ -36,23 +37,26 @@ TEST(RecordCacheTest, PutVariants) {
   rc.PutFromWrite(key1, value);
 
   uint64_t index_out;
-  ASSERT_TRUE(rc.GetIndex(key1, &index_out).ok());
+  ASSERT_TRUE(rc.GetIndex(key1, false, &index_out).ok());
   ASSERT_EQ(value.compare(rc.cache_entries[index_out].GetValue()), 0);
   ASSERT_TRUE(rc.cache_entries[index_out].IsDirty());
   ASSERT_TRUE(rc.cache_entries[index_out].IsWrite());
+  rc.cache_entries[index_out].Unlock();
 
   rc.PutFromRead(key2, value);
 
-  ASSERT_TRUE(rc.GetIndex(key2, &index_out).ok());
+  ASSERT_TRUE(rc.GetIndex(key2, false, &index_out).ok());
   ASSERT_EQ(value.compare(rc.cache_entries[index_out].GetValue()), 0);
   ASSERT_FALSE(rc.cache_entries[index_out].IsDirty());
+  rc.cache_entries[index_out].Unlock();
 
   rc.PutFromDelete(key3);
 
-  ASSERT_TRUE(rc.GetIndex(key3, &index_out).ok());
+  ASSERT_TRUE(rc.GetIndex(key3, false, &index_out).ok());
   ASSERT_EQ(Slice().compare(rc.cache_entries[index_out].GetValue()), 0);
   ASSERT_TRUE(rc.cache_entries[index_out].IsDirty());
   ASSERT_TRUE(rc.cache_entries[index_out].IsDelete());
+  rc.cache_entries[index_out].Unlock();
 }
 
 TEST(RecordCacheTest, MultiPutGet) {
@@ -68,48 +72,17 @@ TEST(RecordCacheTest, MultiPutGet) {
 
   for (auto i = 100; i < 190; ++i) {
     Slice key("a" + std::to_string(i));
-    ASSERT_TRUE(rc.GetIndex(key, &index_out).IsNotFound());
+    ASSERT_TRUE(rc.GetIndex(key, false, &index_out).IsNotFound());
   }
 
   for (auto i = 190; i < 200; ++i) {
     std::string key_s = "a" + std::to_string(i);
     std::string val_s = "b" + std::to_string(i);
 
-    ASSERT_TRUE(rc.GetIndex(Slice(key_s), &index_out).ok());
+    ASSERT_TRUE(rc.GetIndex(Slice(key_s), false, &index_out).ok());
     ASSERT_EQ(Slice(val_s).compare(rc.cache_entries[index_out].GetValue()), 0);
+    rc.cache_entries[index_out].Unlock();
   }
-}
-
-TEST(RecordCacheTest, SimpleLockUnlock) {
-  auto rc = RecordCache(5);
-  Slice key = "aaa";
-  Slice value = "bbb";
-
-  rc.Put(key, value);
-
-  uint64_t index_out;
-  ASSERT_TRUE(rc.GetIndex(key, &index_out).ok());
-  auto entry = rc.cache_entries[index_out];
-
-  entry.Lock(/*exclusive = */ false);
-  ASSERT_EQ(value.compare(entry.GetValue()), 0);
-  entry.Unlock();
-}
-
-TEST(RecordCacheTest, TryLock) {
-  auto rc = RecordCache(5);
-  Slice key = "aaa";
-  Slice value = "bbb";
-
-  rc.Put(key, value);
-
-  uint64_t index_out;
-  ASSERT_TRUE(rc.GetIndex(key, &index_out).ok());
-  auto entry = rc.cache_entries[index_out];
-
-  ASSERT_TRUE(entry.TryLock(/*exclusive = */ false));
-  ASSERT_EQ(value.compare(entry.GetValue()), 0);
-  entry.Unlock();
 }
 
 }  // namespace
