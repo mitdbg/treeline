203b18ab17929f0a7f98fbe2ad6f67b526e9274a
Geoffrey Yu <geoffxy@gmail.com>
2022-02-15 16:01:43 -0500

    Update R/W sweep experiments and scripts (#44)

--- scripts/rw_sweep/plot_e2e.py
+++ scripts/rw_sweep/plot_e2e.py
@@ -1,3 +1,5 @@
+import argparse
+
 import conductor.lib as cond
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -12,51 +14,91 @@ DB_FORMAT = {
 }
 
 
-def plot_e2e(df, distribution, show_legend=False):
+def plot_e2e(
+    df,
+    distribution,
+    record_size,
+    show_legend=False,
+    exclude_100=False,
+    title=None,
+    ylim=None,
+):
     fig, ax = plt.subplots(
         figsize=(6.65, 4),
         tight_layout=True,
         frameon=False,
     )
     dbs = df["db"].unique()
-    # (memtable_mib, cache_mib)
-    configs = [(64, 3200), (3200, 64)]
     for db in dbs:
-        for (memtable_mib, cache_mib) in configs:
-            data = df[(df["db"] == db) &
-                      (df["memtable_mib"] == memtable_mib) &
-                      (df["cache_mib"] == cache_mib) &
-                      (df["dist"] == distribution) &
-                      (df["update_pct"] != 100)]
-            ax.plot(
-                data["update_pct"],
-                data["mops_per_s"] * 1000,
-                marker="o",
-                markersize=5,
-                label="{} W {} / C {}"
-                    .format(DB_FORMAT[db], memtable_mib, cache_mib),
-            )
+        df_filter = (
+            (df["db"] == db)
+            & (df["dist"] == distribution)
+            & (df["record_size_bytes"] == record_size)
+        )
+        if exclude_100:
+            df_filter &= df["update_pct"] != 100
+
+        data = df[df_filter]
+        ax.plot(
+            data["update_pct"],
+            data["krequests_per_s"],
+            marker="o",
+            markersize=5,
+            label=DB_FORMAT[db],
+        )
     if show_legend:
         ax.legend(
             edgecolor="#000000",
             fancybox=False,
             framealpha=1,
         )
-    ax.set_ylabel("Throughput (kops/s)")
-    ax.set_xlabel("Write Percentage (%)")
-    #ax.set_ylim(0, 150)
+    ax.set_ylabel("Throughput (kreq/s)")
+    ax.set_xlabel("Update Percentage (%)")
+    if ylim is not None:
+        ax.set_ylim(ylim)
+    if title is not None:
+        ax.set_title(title)
+
+    if not exclude_100:
+        # Print the value at 100.
+        rel = df[
+            (df["db"] == "rocksdb")
+            & (df["dist"] == distribution)
+            & (df["record_size_bytes"] == record_size)
+            & (df["update_pct"] == 100)
+        ]
+        max_val = rel["krequests_per_s"].iloc[0]
+        fig.text(0.82, 0.94, "{:.2f}".format(max_val))
+
     return fig, ax
 
 
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--distribution", type=str, required=True)
+    parser.add_argument("--record_size_bytes", type=int, required=True)
+    parser.add_argument("--y_min", type=int)
+    parser.add_argument("--y_max", type=int)
+    args = parser.parse_args()
+
+    ylim = None
+    if args.y_min is not None and args.y_max is not None:
+        ylim = (args.y_min, args.y_max)
+
     out_dir = cond.get_output_path()
     df = pd.read_csv(cond.get_deps_paths()[0] / "all_results.csv")
-    for distribution in DISTRIBUTIONS:
-        fig, ax = plot_e2e(
-            df,
-            distribution,
-            show_legend=(distribution == DISTRIBUTIONS[0]))
-        fig.savefig(out_dir / "e2e-{}.pdf".format(distribution), format="pdf")
+    fig, _ = plot_e2e(
+        df,
+        distribution=args.distribution,
+        record_size=args.record_size_bytes,
+        show_legend=True,
+        ylim=ylim,
+    )
+    fig.savefig(
+        out_dir
+        / "rw_sweep-{}-{}.pdf".format(args.distribution, args.record_size_bytes),
+        format="pdf",
+    )
 
 
 if __name__ == "__main__":
--- scripts/rw_sweep/preload.sh
+++ scripts/rw_sweep/preload.sh
@@ -5,58 +5,73 @@ script_loc=$(cd $(dirname $0) && pwd -P)
 cd $script_loc
 source ../experiment_config.sh
 
-if [ -z $1 ]; then
-  >&2 echo "Usage: $0 <checkpoint name> [other args passed to run_custom]"
+# Evaluates any environment variables in this script's arguments. This script
+# should only be run on trusted input.
+orig_args=($@)
+all_args=()
+for val in "${orig_args[@]}"; do
+  phys_arg=$(eval "echo $val")
+
+  # Extract the database type (e.g., llsm, rocksdb, leanstore)
+  if [[ $phys_arg =~ --db=.+ ]]; then
+    db_type=${phys_arg:5}
+  fi
+
+  # Extract the checkpoint name, which shouldn't be passed as an argument further.
+  # Add anything else to args.
+  if [[ $phys_arg =~ --checkpoint_name=.+ ]]; then
+    checkpoint_name=${phys_arg:18}
+  else
+    all_args+=($phys_arg)
+  fi
+done
+
+if [[ -z $db_type || -z $checkpoint_name ]]; then
+  echo >&2 "Usage: $0 --checkpoint_name=<checkpoint name> --db=<all|rocksdb|llsm> [other args passed to run_custom]"
   exit 1
 fi
-
-checkpoint_name=$1
-shift 1
 
 full_checkpoint_path=$DB_CHECKPOINT_PATH/$checkpoint_name
 
 # Check if the checkpoint already exists. If so, we do not need to rerun.
 if [ -d "$full_checkpoint_path" ]; then
-  >&2 echo "Checkpoint $checkpoint_name already exists. No need to recreate."
+  echo >&2 "Checkpoint $checkpoint_name already exists. No need to recreate."
   exit 0
 fi
-
-# Evaluates any environment variables in this script's arguments. This script
-# should only be run on trusted input.
-orig_args=$@
-all_args=()
-for val in "${orig_args[@]}"; do
-  all_args+=$(eval "echo $val")
-done
 
 mkdir -p $DB_CHECKPOINT_PATH
 
 # Generates the workload configuration and filters out unrelated arguments.
-args=$(python3 generate_workload.py $all_args)
+args=$(python3 generate_workload.py ${all_args[@]})
 
-# RocksDB - Important to use 64 MiB memtables to ensure the emitted sstables
-#           are small too.
-../../build/bench/run_custom \
-  --db=rocksdb \
-  --db_path=$full_checkpoint_path \
-  --bg_threads=16 \
-  --bypass_wal=true \
-  --memtable_size_mib=64 \
-  --workload_config=$COND_OUT/workload.yml \
-  --use_direct_io=true \
-  --seed=$SEED \
-  --verbose \
-  $args
+if [ $db_type == "rocksdb" ] || [ $db_type == "all" ]; then
+  # RocksDB - Important to use 64 MiB memtables to ensure the emitted sstables
+  #           are small too.
+  ../../build/bench/run_custom \
+    --db=rocksdb \
+    --db_path=$full_checkpoint_path \
+    --bg_threads=16 \
+    --bypass_wal=true \
+    --memtable_size_mib=64 \
+    --workload_config=$COND_OUT/workload.yml \
+    --use_direct_io=true \
+    --seed=$SEED \
+    --verbose \
+    --rdb_bloom_bits=10 \
+    ${args[@]}
+fi
 
-# LLSM - Use a larger memtable to help the load run faster.
-../../build/bench/run_custom \
-  --db=llsm \
-  --db_path=$full_checkpoint_path \
-  --bg_threads=16 \
-  --bypass_wal=true \
-  --llsm_page_fill_pct=50 \
-  --memtable_size_mib=2048 \
-  --workload_config=$COND_OUT/workload.yml \
-  --seed=$SEED \
-  --verbose \
-  $args
+if [ $db_type == "llsm" ] || [ $db_type == "all" ]; then
+  # LLSM - Use a larger memtable to help the load run faster.
+  ../../build/bench/run_custom \
+    --db=llsm \
+    --db_path=$full_checkpoint_path \
+    --bg_threads=16 \
+    --bypass_wal=true \
+    --llsm_page_fill_pct=70 \
+    --workload_config=$COND_OUT/workload.yml \
+    --seed=$SEED \
+    --verbose \
+    --use_alex=false \
+    ${args[@]}
+fi
