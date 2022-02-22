03b474197fa49a74688cfb3f017ca2a9d31e78a6
Geoffrey Yu <geoffxy@mit.edu>
2022-02-22 16:10:48 -0500

    Make workloads templated

---
+++ scripts/ycsb_v2/generate_workload.py
@@ -0,0 +1,51 @@
+import argparse
+import conductor.lib as cond
+
+ZIPFIAN_DIST = """\
+      type: zipfian
+      theta: 0.99\
+"""
+
+UNIFORM_DIST = """\
+      type: uniform\
+"""
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--gen_template", type=str, required=True)
+    parser.add_argument("--gen_num_requests", type=int, required=True)
+    parser.add_argument("--gen_range_min", type=int, required=True)
+    parser.add_argument("--gen_range_max", type=int, required=True)
+    parser.add_argument(
+        "--gen_distribution", choices=["zipfian", "uniform"], default="zipfian"
+    )
+    args, unknown = parser.parse_known_args()
+
+    # Load the template.
+    with open(args.gen_template, "r") as template_file:
+        template = template_file.read()
+
+    # If the template does not contain a key in the config, the config value is
+    # just ignored.
+    config = {
+        "range_min": args.gen_range_min,
+        "range_max": args.gen_range_max,
+        "num_requests": args.gen_num_records,
+        "distribution": ZIPFIAN_DIST
+        if args.gen_distribution == "zipfian"
+        else UNIFORM_DIST,
+    }
+    workload = template.format(**config)
+
+    # Write out the generated workload.
+    out_dir = cond.get_output_path()
+    with open(out_dir / "workload.yml", "w") as file:
+        file.write(workload)
+
+    # Pass through unknown arguments.
+    print(" ".join(unknown))
+
+
+if __name__ == "__main__":
+    main()
---
+++ scripts/ycsb_v2/run.sh
@@ -0,0 +1,105 @@
+#! /bin/bash
+set -e
+
+script_loc=$(cd $(dirname $0) && pwd -P)
+cd $script_loc
+source ../experiment_config.sh
+
+# Evaluates any environment variables in this script's arguments. This script
+# should only be run on trusted input.
+orig_args=($@)
+all_args=()
+for val in "${orig_args[@]}"; do
+  phys_arg=$(eval "echo $val")
+
+  # Extract the database type (e.g., llsm, rocksdb, leanstore)
+  if [[ $phys_arg =~ --db=.+ ]]; then
+    db_type=${phys_arg:5}
+  fi
+
+  # Extract the checkpoint name, which shouldn't be passed as an argument further.
+  # Add anything else to args.
+  if [[ $phys_arg =~ --checkpoint_name=.+ ]]; then
+    checkpoint_name=${phys_arg:18}
+  else
+    all_args+=($phys_arg)
+  fi
+done
+
+if [[ -z $checkpoint_name ]]; then
+  echo >&2 "Usage: $0 --checkpoint_name=<checkpoint name> [other args passed to run_custom]"
+  exit 1
+fi
+
+echo >&2 "Detected DB Type: $db_type"
+echo >&2 "Detected checkpoint name: $checkpoint_name"
+
+# Generate the workload configuration file and filter out unrelated arguments.
+# The generated workload configuration file will be saved to $COND_OUT/workload.yml.
+args=$(python3 generate_workload.py ${all_args[@]})
+
+# Add common arguments.
+args+=("--verbose")
+args+=("--db_path=$DB_PATH")
+args+=("--seed=$SEED")
+args+=("--notify_after_init")
+
+full_checkpoint_path=$DB_CHECKPOINT_PATH/$checkpoint_name
+rm -rf $DB_PATH
+
+# LeanStore does not support "reopening" a database. So we do not have
+# checkpoints for LeanStore. Note that we currently **do not** run the "setup"
+# workload on LeanStore (10 M uniform updates).
+if [[ $db_type == "leanstore" ]]; then
+  mkdir $DB_PATH
+else
+  args+=("--skip_load")
+  cp -r $full_checkpoint_path $DB_PATH
+fi
+
+sync $DB_PATH
+
+init_finished=0
+
+function on_init_finish() {
+  init_finished=1
+}
+
+# Interrupt the first `wait` below when we receive a `SIGUSR1` signal.
+trap "on_init_finish" USR1
+
+set +e
+../../build/bench/run_custom \
+  ${args[@]} \
+  --workload_config=$COND_OUT/workload.yml \
+  --output_path=$COND_OUT \
+  > $COND_OUT/results.csv &
+wait %1
+code=$?
+
+# The experiment failed before it finished initialization.
+if [ "$init_finished" -eq "0" ]; then
+  exit $code
+fi
+
+# The DB has finished initializing, so start `iostat`.
+iostat -o JSON -d -y 1 >$COND_OUT/iostat.json &
+iostat_pid=$!
+
+# Wait until the workload completes.
+wait %1
+code=$?
+
+# Stop `iostat`.
+kill -s SIGINT -- $iostat_pid
+wait
+
+if [ -e $DB_PATH/$db_type/LOG ]; then
+  cp $DB_PATH/$db_type/LOG $COND_OUT/$db_type.log
+fi
+du -b $DB_PATH >$COND_OUT/db_space.log
+
+# Report that the experiment failed if the `run_custom` exit code is not 0
+if [ $code -ne 0 ]; then
+  exit $code
+fi
