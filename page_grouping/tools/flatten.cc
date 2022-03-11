8050e7bcf0a9071bdb30a0a099db3ab25b40860c
Geoffrey Yu <geoffxy@mit.edu>
2022-03-11 15:03:56 -0500

    Add tool that calls flatten

---
+++ page_grouping/tools/flatten.cc
@@ -0,0 +1,44 @@
+#include <cstdint>
+#include <iostream>
+#include <limits>
+
+#include "gflags/gflags.h"
+#include "llsm/pg_db.h"
+
+DEFINE_string(db_path, "", "Path to the database directory.");
+DEFINE_uint64(
+    start_key, 0,
+    "The lower boundary of the key space to pass to `FlattenRange()`.");
+DEFINE_uint64(end_key, std::numeric_limits<uint64_t>::max(),
+              "The upper boundary (exclusive) of the key space to pass to "
+              "`FlattenRange()`.");
+DEFINE_uint32(goal, 45,
+              "Passed to PageGroupedDBOptions::records_per_page_goal.");
+DEFINE_uint32(delta, 5,
+              "Passed to PageGroupedDBOptions::records_per_page_delta.");
+
+using namespace llsm;
+using namespace llsm::pg;
+
+int main(int argc, char* argv[]) {
+  gflags::SetUsageMessage("Runs FlattenRange() on a page-grouped database.");
+  gflags::ParseCommandLineFlags(&argc, &argv, /*remove_flags=*/true);
+  if (FLAGS_db_path.empty()) {
+    std::cerr << "ERROR: Must provide a database path." << std::endl;
+    return 1;
+  }
+
+  PageGroupedDBOptions options;
+  options.use_segments = true;
+  options.records_per_page_goal = FLAGS_goal;
+  options.records_per_page_delta = FLAGS_delta;
+  options.use_memory_based_io = true;
+
+  PageGroupedDB* db = nullptr;
+  PageGroupedDB::Open(options, FLAGS_db_path, &db);
+  assert(db != nullptr);
+  db->FlattenRange(FLAGS_start_key, FLAGS_end_key);
+  delete db;
+
+  return 0;
+}
