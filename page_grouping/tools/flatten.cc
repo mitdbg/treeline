95a5decd827eb0b10884e3fc509322da6b83ef06
Geoffrey Yu <geoffxy@mit.edu>
2022-08-11 09:58:26 -0400

    Fix incorrect invocation of the pg_flatten tool (#93)
    * Fix usage of boundary keys in the flatten tool
    
    * Fixes to the experiment script

--- page_grouping/tools/flatten.cc
+++ page_grouping/tools/flatten.cc
@@ -7,7 +7,7 @@
 
 DEFINE_string(db_path, "", "Path to the database directory.");
 DEFINE_uint64(
-    start_key, 0,
+    start_key, 1,
     "The lower boundary of the key space to pass to `FlattenRange()`.");
 DEFINE_uint64(end_key, std::numeric_limits<uint64_t>::max(),
               "The upper boundary (exclusive) of the key space to pass to "
@@ -34,10 +34,14 @@ int main(int argc, char* argv[]) {
   options.records_per_page_epsilon = FLAGS_epsilon;
   options.use_memory_based_io = true;
 
+  std::cerr << "Flattening PGTreeLine DB at: " << FLAGS_db_path << std::endl;
   PageGroupedDB* db = nullptr;
   PageGroupedDB::Open(options, FLAGS_db_path, &db);
   assert(db != nullptr);
-  db->FlattenRange(FLAGS_start_key, FLAGS_end_key);
+  const auto status = db->FlattenRange(FLAGS_start_key, FLAGS_end_key);
+  if (!status.ok()) {
+    throw std::runtime_error(status.ToString());
+  }
   delete db;
 
   return 0;
